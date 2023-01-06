3d6d6386c2e7f64c31c0da1cae2485495597998d
rhksdn0731 <rhksdn0731@naver.com>
2023-01-06 23:04:07 +0900

    day 6
    component의 쓰임새에 따른 구분

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -9,26 +9,33 @@ import {
 import Root from "./pages/UsememoUseEffect";
 import RouteTest from "./pages/RouteTest";
 import ErrorPage from "./pages/ErrorPage";
+import TestComponent from "./pages/TestComponent";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Root/>,
-    errorElement: <ErrorPage />, //v6 부터 추가된 기능입니다.
+    path: "/", // path입니다.
+    element: <Root/>, // 이동할 대상입니다.
+    errorElement: <ErrorPage />, //v6 부터 추가된 기능입니다. 에러 발생 시 이동할 곳을 지정할 수 있습니다.
   },
   {
-    path: "RouteTest/:testParam",
+    path: "RouteTest/:testParam", //이와 같이 유동적인 pramater를 받을 수 있습니다.
     element: <RouteTest />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "ClassComponent", //이와 같이 유동적인 pramater를 받을 수 있습니다.
+    element: <TestComponent />,
     errorElement: <ErrorPage />,
   },
 ]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
-root.render(
+); // root를 HTMLElement로 #root에 생성할 것을 선언합니다. 택배 송장입니다.
+
+root.render( // 생성된 root를 랜더링합니다. 택배를 발송합니다.
   <React.StrictMode>
-   <RouterProvider router={router} />
+   <RouterProvider router={router} /> {/*RouterProvider에서 router를 받아 읽고 실행합니다.*/}
   </React.StrictMode>
 );
 
