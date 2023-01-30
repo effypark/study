d10d423a78dbf8b4d871ff42582537d2e0458fac
seohee <hecarisweat@gmail.com>
2023-01-30 23:59:25 +0900

    Add insert

--- seohee/demo/src/main/java/com/example/demo/controller/UserController.java
+++ seohee/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,33 +1,31 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.UserDto;
-import com.example.demo.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import com.example.demo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController // view 페이지가 필요없는 API 응답에 어울리는 어노테이션입니다.
-@RequiredArgsConstructor
-
 public class UserController {
-    private final UserService userService;
+    private final UserMapper userMapper;
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public List<UserDto> getUser() {
-        return userService.getUserList();
+    @Autowired
+    public UserController(UserMapper userMapper) {
+        this.userMapper = userMapper;
     }
 
-    @RequestMapping(value = "/active-user", method = RequestMethod.GET)
-    public List<UserDto> getActiveUser() {
-        return userService.getActiveUserList();
+    @PutMapping("/join")
+    public void insertUser(@RequestParam("username") String username, @RequestParam("password") String password) {
+        userMapper.insertUser(username, password);
     }
-
-//    @PostMapping("/join")
-//    public int insertUser(UserDto userDto) {
-//        return userService.insertUser(userDto);
+//    @RequestMapping(value = "/user", method = RequestMethod.GET)
+//    public List<UserDto> getUser() {
+//        return userService.getUserList();
+//    }
+//
+//    @RequestMapping(value = "/active-user", method = RequestMethod.GET)
+//    public List<UserDto> getActiveUser() {
+//        return userService.getActiveUserList();
 //    }
 }
