f6340323008c866d31922088432ecddf95600813
seohee <hecarisweat@gmail.com>
2023-01-31 23:56:32 +0900

    Add delete

--- seohee/demo/src/main/java/com/example/demo/controller/UserController.java
+++ seohee/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -2,9 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // view 페이지가 필요없는 API 응답에 어울리는 어노테이션입니다.
 public class UserController {
@@ -18,6 +16,11 @@ public class UserController {
     @PutMapping("/join")
     public void insertUser(@RequestParam("username") String username, @RequestParam("password") String password) {
         userMapper.insertUser(username, password);
+    }
+
+    @DeleteMapping("/delete/{idx}")
+    public void deleteUser(@PathVariable String idx) {
+        userMapper.deleteUser(idx);
     }
 //    @RequestMapping(value = "/user", method = RequestMethod.GET)
 //    public List<UserDto> getUser() {
--- seohee/demo/src/main/java/com/example/demo/mapper/UserMapper.java
+++ seohee/demo/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.example.demo.mapper;
 
 import com.example.demo.dto.UserDto;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -15,4 +16,7 @@ public interface UserMapper {
 
     @Insert("INSERT INTO users(username, password) VALUES (#{username}, #{password})")
     int insertUser(@Param("username") String username, @Param("password") String password);
+
+    @Delete("DELETE FROM users where idx = ${idx}")
+    int deleteUser(@Param("idx") String idx);
 }
