945cbd54f70f1717d09415c92d8aa7a6468b93b6
seohee <hecarisweat@gmail.com>
2023-02-08 00:04:41 +0900

    WIP

--- seohee/demo/src/main/java/com/example/demo/entity/UserEntity.java
+++ seohee/demo/src/main/java/com/example/demo/entity/UserEntity.java
@@ -1,6 +1,25 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+
 // 일종의 테이블 역할
 public class UserEntity {
+    @Id // pk 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long id;
 
+    @Column(unique = true) // unique 걸기
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String active;
 }
--- seohee/demo/src/main/java/com/example/demo/service/UserService.java
+++ seohee/demo/src/main/java/com/example/demo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.UserDto;
+import com.example.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
