e04061c8eb08db299a75e96d49c1c5869098b3a1
seohee <hecarisweat@gmail.com>
2023-02-28 23:57:24 +0900

    Add readme

---
+++ seohee/demo/src/main/java/com/example/demo/controller/BoardController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @GetMapping("/")
+    public String list() {
+        return "list";
+    }
+}
