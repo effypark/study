156790e90c2e4595dbda77ab7e36d22d3e79587f
rhksdn0731 <rhksdn0731@naver.com>
2023-03-17 23:10:12 +0900

    Day-73

--- app/src/Components/CustomSelect.tsx
+++ app/src/Components/CustomSelect.tsx
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
+import React, { FC, useState } from 'react';
 
-class CustomSelect extends Component {
-    render() {
-        return (
-            <>
-             <p>test</p>
-            </>
-        );
-    }
-}
+type SelectProps = {
+  items: string[];
+  onSelect: (selectedItem: string) => void;
+};
 
+const CustomSelect: FC<SelectProps> = ({ items, onSelect }) => {
+  const [selectedItem, setSelectedItem] = useState('');
+
+  const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = event.target.value;
+    setSelectedItem(selectedValue);
+    onSelect(selectedValue);
+  };
+
+  return (
+    <select value={selectedItem} onChange={handleSelect}>
+      {items.map((item) => (
+        <option key={item} value={item}>
+          {item}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+
+  );
+
+ 
 export default CustomSelect;
