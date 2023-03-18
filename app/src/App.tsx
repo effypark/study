1842d55dce002779b88397c23ca8aa6222ab8172
rhksdn0731 <rhksdn0731@naver.com>
2023-03-18 22:24:06 +0900

    Day-74

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,12 +3,17 @@ import { BrowserRouter, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+import { Provider } from 'react-redux';
+import store from './store/config';
+
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes/>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes/>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ studyDocument/kyj/230318_vaildation.md
@@ -0,0 +1,2 @@
+# react-form-hook & Yup
+
